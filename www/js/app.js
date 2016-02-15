fe32f050e93fe1141f809c2592df6497a185743e
Heleriin Ots <heleot@ttu.ee>
2016-02-15 23:06:20 +0200

    #113302315 I removed unnecessary default tabs.

--- www/js/app.js
+++ www/js/app.js
@@ -38,33 +38,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngMessages'])
       controller: 'AppCtrl'
     })
 
-    .state('app.search', {
-      url: '/search',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/search.html'
-        }
-      }
-    })
-
-    .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
     .state('app.tournamentFeed', {
       url: '/tournamentFeed',
       views: {
--- www/js/playlistCtrl.js
+++
@@ -1,5 +0,0 @@
-angular.module('starter.controllers')
-    .controller('PlaylistCtrl', function ($scope, $stateParams) {
-
-        //TODO: enter some JS magic
-    });
