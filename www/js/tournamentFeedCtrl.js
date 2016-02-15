1ab835e2d433bc0362a1877f150dbbda6c5dd754
Karl Mae <karl.m2e@gmail.com>
2016-02-15 20:10:45 +0200

    #109292042 (multiple) cards in tournament feed are now fetched through API

--- www/js/tournamentFeedCtrl.js
+++ www/js/tournamentFeedCtrl.js
@@ -1,4 +1,11 @@
 angular.module('starter.controllers')
-    .controller('TournamentFeedCtrl', function ($scope) {
-
+    .controller('TournamentFeedCtrl', function ($scope, $http) {
+     $http.get('http://188.166.104.203:7070/rest/card')
+        .then(function(data) {
+             $scope.cards = data.data;
+             console.log('Success', data);
+             }, function(err) {
+              console.error('ERR', err);
+            // err.status will contain the status code
+        });
     });
