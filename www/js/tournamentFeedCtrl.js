b4f3d77b46a9a0e63dcdb2939cc747855dd0a60f
Ain-Joonas toose <Ain.Joonas.Toose@gmail.com>
2016-02-20 19:52:42 +0200

    Made the card info pollable in a way that loads itself on start before starting to poll on an interval. Also added some start material to the HTML to make the card swipable(testing code on the function commented out)

--- www/js/tournamentFeedCtrl.js
+++ www/js/tournamentFeedCtrl.js
@@ -1,16 +1,24 @@
 angular.module('starter.controllers')
-    .controller('TournamentFeedCtrl', function ($scope, $http, authenticatedUserService, $state, $interval) {
+    .controller('TournamentFeedCtrl', function ($scope, $http, authenticatedUserService, $state, $interval, $ionicGesture) {
+        var getCardData = function() {
+             $http.get('http://localhost:7070/rest/card')
+            .then(function (data) {
+                $scope.cards = data.data;
+                console.log('Success', data);
+            }, function (err) {
+                console.error('ERR', err);
+                // err.status will contain the status code
+            });
+        }
+        //  $http.get('http://188.166.104.203:7070/rest/card')
+        getCardData();
+        $interval(getCardData,2000);
 
 
-   //  $http.get('http://188.166.104.203:7070/rest/card')
-   this.interval = $interval(function(){
-     $http.get('http://localhost:7070/rest/card')
-        .then(function(data) {
-             $scope.cards = data.data;
-             console.log('Success', data);
-             }, function(err) {
-              console.error('ERR', err);
-            // err.status will contain the status code
-        });
-    },2000);
+ /*       $scope.onSwipeLeft = function () {
+            console.log("Swiped left");
+            $scope.cardDestroyed = function (index) {
+                $scope.cards.splice(index, 1);
+            };
+        } */
     });
