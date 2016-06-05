e966f3b8039d45c202f4ea96eeecb0aad7e1e597
Karl Mae <karl.m2e@gmail.com>
2016-06-05 11:35:06 +0300

    #109298116 when user navigates to maps view, a google maps is loaded and displayed

--- www/js/mapCtrl.js
+++ www/js/mapCtrl.js
@@ -1,5 +1,23 @@
 angular.module('starter.controllers')
 
-.controller('MapCtrl', function($scope, $state, $stateParams) {
+.controller('MapCtrl', function($scope, $state, $cordovaGeolocation) {
+    
+    var options = {timeout: 10000, enableHighAccuracy: true};
+ 
+  $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+ 
+    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+ 
+    var mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+ 
+    $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
+  }, function(error){
+    console.log("Could not get location");
+  });
+    
 });
