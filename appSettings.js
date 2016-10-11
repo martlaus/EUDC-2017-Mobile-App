205985cb3e2b38ffeee460a12fb4879d6c1dd337
wanaryytel <hanno@thorgate.eu>
2016-10-11 21:51:00 +0300

    fixed untracked files

--- appSettings.js
+++ appSettings.js
@@ -1,6 +1,6 @@
 AppSettings = {
     // @if ENV == 'DEVELOPMENT'
-    baseApiUrl: 'http://localhost:7070/',
+    // baseApiUrl: 'http://localhost:7070/',
     debug: true,
     // @endif
     // @if ENV == 'TEST'
