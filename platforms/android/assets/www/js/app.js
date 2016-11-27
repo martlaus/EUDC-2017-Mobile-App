fb74aed5b8817cd9a1383def89f939930be8eef5
Mart Laus <mart.laus@ttu.ee>
2016-11-27 17:13:20 +0200

    #109298994 Add sending question

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -4,70 +4,113 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'timer', 'ngMessages', 'ngCordova', 'starter.controllers'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
-
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
-
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
     })
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
-});
+    .run(['$rootScope', 'authenticatedUserService', '$location', function ($rootScope, authenticatedUserService, $location) {
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            var values = next.split('#');
+            if (!authenticatedUserService.isAuthenticated() && values[1] && values[1] !== "/login") {
+                $location.path("/login");
+            }
+        });
+    }])
+
+
+    .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+        $stateProvider
+
+            .state('login', {
+                url: '/login',
+                templateUrl: 'templates/login.html',
+                controller: 'LoginCtrl'
+            })
+
+            .state('app', {
+                url: '/app',
+                abstract: true,
+                templateUrl: 'templates/menu.html',
+                controller: 'AppCtrl'
+            })
+
+            .state('app.tournamentFeed', {
+                url: '/tournamentFeed',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/tournamentFeed.html',
+                        controller: 'TournamentFeedCtrl'
+                    }
+                }
+            })
+
+            .state('app.card', {
+                url: '/cards/:id',
+                params: {
+                    'card': null
+                },
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/card.html',
+                        controller: 'CardCtrl'
+                    }
+                }
+            })
+
+            .state('app.map', {
+                url: '/map',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/map.html',
+                        controller: 'MapCtrl'
+                    }
+                }
+            })
+
+            .state('app.contacts', {
+                url: '/contacts',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/contacts.html',
+                        controller: 'contactsCtrl'
+                    }
+                }
+            })
+
+            .state('app.about', {
+                url: '/about',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/about.html',
+                        controller: 'aboutCtrl'
+                    }
+                }
+            })
+
+            .state('app.calendar', {
+                url: '/calendar',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/calendar.html',
+                        controller: 'CalendarCtrl'
+                    }
+                }
+            });
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/login');
+
+    });
--- www/js/contactsCtrl.js
+++ www/js/contactsCtrl.js
@@ -1,5 +1,36 @@
 angular.module('starter.controllers')
 
-    .controller('contactsCtrl', function($scope, $state) {
+    .controller('contactsCtrl', function ($scope, $state, serverCallService, $ionicPopup, $timeout) {
 
+        $scope.feedback = {};
+
+        $scope.sendFeedback = function () {
+            serverCallService.makePost(AppSettings.baseApiUrl + "rest/feedback", $scope.feedback, success, error);
+        };
+
+        function success(data) {
+            $scope.feedback = null;
+            showPopup();
+        }
+
+        function error() {
+            console.log("Failed to send feedback");
+        }
+
+        function showPopup() {
+            var myPopup = $ionicPopup.show({
+                template: 'Thank you for your questions and feedback!',
+                title: 'Feedback sent!',
+                buttons: [
+                    {
+                        text: '<b>Close</b>',
+                        type: 'button-positive',
+                    }
+                ]
+            });
+
+            $timeout(function () {
+                myPopup.close(); //close the popup after 4 seconds for some reason
+            }, 4000);
+        }
     });
