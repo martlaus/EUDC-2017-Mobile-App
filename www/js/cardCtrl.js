70bd1f2b1d772d945174b485a09eede9ee726f74
Mart Laus <mart.laus@ttu.ee>
2017-02-28 19:47:24 +0200

    Refactoring

--- www/js/cardCtrl.js
+++ www/js/cardCtrl.js
@@ -4,13 +4,12 @@ angular.module('starter.controllers')
 
         var logos = ["./img/TTU_alternatiivne_logo_EST_ENG_veeb.png", "./img/tutDebateClub.png", "./img/evs_eng.png"];
 
-
         showRandomLogo();
         showInfoOnCard();
 
         $scope.trustHtml = function(src) {
             return $sce.trustAsHtml(src);
-        }
+        };
 
         function showRandomLogo() {
             var srcLogo = logos[(Math.floor(Math.random() * logos.length) + 0)];
@@ -18,23 +17,7 @@ angular.module('starter.controllers')
         }
 
         function showInfoOnCard() {
-
-            var id = findVideoId();
-
             $scope.card = $stateParams.card;
-        }
-
-        function findVideoId() {
-            var description = $stateParams.card.description;
-
-            var id = '';
-            var url = description.replace(/(>|<)/gi, '').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
-
-            if (url[2] !== undefined) {
-                id = url[2].split(/[^0-9a-z_\-]/i);
-                id = id[0];
-            }
-            return id;
         }
 
         function createVideoIframe(link) {
