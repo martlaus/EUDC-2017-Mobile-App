66d497b873acad4337d30e47d34c48cdb2180141
Mart Laus <mart.laus@ttu.ee>
2017-08-12 17:36:33 +0300

    Refactor barcode

--- www/js/profileCtrl.js
+++ www/js/profileCtrl.js
@@ -5,37 +5,30 @@ angular.module('starter.controllers')
         $scope.user = authenticatedUserService.getUser();
         $scope.role = $scope.user.role.toLowerCase();
         $scope.barcode = "data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==";
-        $scope.loading = false;
+        $scope.loading = true;
+        getBarcode();
 
         $scope.logOut = function () {
             showLogoutAlert();
         };
 
-        $scope.getLoadingClass = function () {
-            if (!$scope.loading) {
-                return '';
-            } else {
-                return 'loading';
-            }
-        };
+        function getBarcode() {
+            var item = localStorage.getItem("barCode");
+            if (!item) {
+                var user = authenticatedUserService.getUser();
 
-        $scope.getBarcode = function () {
-            if (!localStorage.barCode && !$scope.loading) {
-                var tabbieId = JSON.parse(localStorage.authenticatedUser).user.tabbieId; // TODO change tournament id
-                $scope.loading = true;
-
-                serverCallService.makeGet(AppSettings.baseApiUrl + "rest/barcode/" + tabbieId, {}, function (data) {
-                    $scope.barcode = 'data:image/png;base64, ' + data.b64;
-                    localStorage.setItem('barCode', $scope.barcode);
+                serverCallService.makeGet(AppSettings.baseApiUrl + "rest/barcode/" + user.tabbieId, {}, function (data) {
+                    if (data.b64) {
+                        $scope.barcodeImg = 'data:image/png;base64, ' + data.b64;
+                        localStorage.setItem('barCode', $scope.barcodeImg);
+                        $scope.getBarcode(); // Hacky thingie that gets over the angular scope updating problem that loading isn't set to false in the callback (if you know a better way, lemme know)
+                    }
                     $scope.loading = false;
-                    $scope.getBarcode(); // Hacky thingie that gets over the angular scope updating problem that loading isn't set to false in the callback (if you know a better way, lemme know)
                 }, error);
             } else if (!$scope.loading) {
                 $scope.loading = false;
-                $scope.barcode = localStorage.barCode;
+                $scope.barcodeImg = item;
             }
-
-            return $scope.barcode;
         };
 
 
@@ -59,8 +52,9 @@ angular.module('starter.controllers')
             window.location.reload();
         }
 
-
         function error() {
+            $scope.loading = false;
+
             var alertPopup = $ionicPopup.alert({
                 title: 'Login failed',
                 template: 'Please try again.'
